1d9d0ca5167258aabc45ad426359ef3bbc7b2182
Stefan Pijnappels <stefan@puppet.com>
2017-01-16 11:54:06 +0000

    dst hack
    This hack adds DST support through manipulating the NTP_DELTA value,
    subtracting 3600 seconds for when DST is active.
    Suggested method to use is to run settime(), check date values and if
    values fall within DST range, run settime(dst).
    TODO: write wrapper function to do this.

--- esp8266/scripts/ntptime.py
+++ esp8266/scripts/ntptime.py
@@ -7,12 +7,11 @@ try:
 except:
     import struct
 
-# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
-NTP_DELTA = 3155673600
+dst = 1
 
 host = "pool.ntp.org"
 
-def time():
+def time(NTP_DELTA):
     NTP_QUERY = bytearray(48)
     NTP_QUERY[0] = 0x1b
     addr = socket.getaddrinfo(host, 123)[0][-1]
@@ -26,8 +25,16 @@ def time():
 
 # There's currently no timezone support in MicroPython, so
 # utime.localtime() will return UTC time (as if it was .gmtime())
-def settime():
-    t = time()
+# Small change to enable DST (essentially BST until TZ support)
+# by manipulating NTP_DELTA value which is derived from following:
+# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
+# To use DST, call function again as settime(dst)
+def settime( dst = 0 ):
+    if dst == 1:
+        NTP_DELTA = 3155670000
+    else:
+        NTP_DELTA = 3155673600
+    t = time(NTP_DELTA)
     import machine
     import utime
     tm = utime.localtime(t)
